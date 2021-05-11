b386b71191afc4f1b6fa18e9ca5fdf5e54adad50
Dominique Sakrisson <domsakrisson@gmailcom>
2021-05-10 19:33:42 -0700

    working with the babypicture

--- src/containers/ColorGen.jsx
+++ src/containers/ColorGen.jsx
@@ -4,38 +4,28 @@ import Display from '../components/color/Display';
 
 export default class ColorGen extends Component{
     state ={
-        // color: '#FF0000',
         colors: [0,0,255],
         opacity: 0,
     }
-   
+   //20 ms refresh rate, check out the console for the baby console log
     componentDidMount(){
         let colors = [
             Math.round(Math.random() * 255),
             Math.round(Math.random() * 255),
             Math.round(Math.random() * 255)
         ];
-        setTimeout(()=>{
+        // setTimeout(()=>{
             this.setState({colors: colors});
-        }, 2000);
-        
-    }
-    componentDidUpdate(){
-        let colors = [
-            Math.round(Math.random() * 255),
-            Math.round(Math.random() * 255),
-            Math.round(Math.random() * 255)
-            //shows that the baby picture will show if these numbers ever miraculously all land on the same value twice
-            //uncomment the line below and comment out the 3 lines above
-            // 255,255,255
-        ];
+        // }, 2000);
         //for some reason the timeout eventually breaks and the whole square just goes chaotic
-        setTimeout(()=>{
+        setInterval(()=>{
+            let color = this.randomColor();
             //creates an array thats a copy of current color roll and the color in state
-            let a = colors;
+            let a = color;
             let b = this.state.colors;
             let correct = 0;
-            //loops through 3 times, the length of the rba values array
+
+            // loops through 3 times, the length of the rba values array
             for(let i =0; i < b.length; i++){
                 if(a[i] === b[i]){
                     correct++;
@@ -43,20 +33,30 @@ export default class ColorGen extends Component{
                 //if its correct 3 times, the rgb color matches the previous one
                 if(correct === 3) {
                     //they match so set opacity to 1, showing the picture
+                    console.log('babypicture');
                     this.setState({opacity: 1});
+                    correct = 0;
+                    // this.setState({colors: this.randomColor()})
                 } else {
-                    this.setState({colors: colors});
+                    this.setState({opacity: 0})
+                    this.setState({colors: this.randomColor()});
                 }
             }
-            //bringing this code back in
-            // if(colors == this.state.colors){
-            //     this.setState({opacity: 1})
-            //     this.setState({colors: colors})
-                
-            // } else {
-            //     this.setState({colors: colors});
-            // }
-        }, 2000);
+        }, 20);
+        
+    }
+
+    randomColor = () => {
+        //so many colors, good luck finding the baby picture
+        let color = [
+            Math.floor(Math.random() * 45),
+            150,
+            Math.floor(Math.random() * 45)
+            //shows that the baby picture will show if these numbers ever miraculously all land on the same value twice
+            // uncomment the line below and comment out the 3 lines above
+            // 255,255,255
+        ];
+        return color;
     }
 
     render(){
